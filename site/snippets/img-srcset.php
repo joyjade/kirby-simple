9810c5408ae0bc7a17d67b47a00e82f952a3189f
JJ <jadeandpearls@gmail.com>
2025-09-15 17:44:44 -0600

    add img srcset and config for local vs prod

---
+++ site/snippets/img-srcset.php
@@ -0,0 +1,23 @@
+<figure>
+  <?php if($max == 'full') : ?>
+    <img
+        loading="eager"
+        alt="<?= $photo->alt() ?>"
+        src="<?= $photo->resize(400)->url() ?>"
+        srcset="<?= $photo->srcset([400, 600, 900, 1200, 1800]) ?>"
+        width="<?= $photo->resize(1800)->width() ?>"
+        height="<?= $photo->resize(1800)->height() ?>"
+        aria-describedby="materials"
+    >
+  <?php else : ?>
+    <img
+      loading="lazy"
+      alt="<?= $photo->alt() ?>"
+      src="<?= $photo->resize(400)->url() ?>"
+      srcset="<?= $photo->srcset([400, 600, 900]) ?>"
+      width="<?= $photo->resize(900)->width() ?>"
+      height="<?= $photo->resize(900)->height() ?>"
+      aria-describedby="materials"
+    >
+  <?php endif ?>
+</figure>
